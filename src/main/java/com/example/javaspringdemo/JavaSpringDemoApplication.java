ff44bec4a85b6db15fa4380d9fd2f064ec882500
harunugurlu <harunugurlu1@gmail.com>
2024-04-05 22:19:56 +0300

    add hello world get handler

--- src/main/java/com/example/javaspringdemo/JavaSpringDemoApplication.java
+++ src/main/java/com/example/javaspringdemo/JavaSpringDemoApplication.java
@@ -2,12 +2,22 @@ package com.example.javaspringdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class JavaSpringDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaSpringDemoApplication.class, args);
     }
 
+    @GetMapping
+    public List<String> hello() {
+        return List.of("Hello there!", "Welcome to the app.");
+    }
 }
